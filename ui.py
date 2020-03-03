f6a7d0bdec2b018c1a1d8afdc1a397ef6281b40a
Chris Overly <cqoverly@gmail.com>
2020-03-02 20:37:53 -0800

    Added rough layout to ui.py for entry tab

--- ui.py
+++ ui.py
@@ -29,6 +29,53 @@ class MainManager(qtw.QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Set up base tab widget
+        tab_controller = qtw.QTabWidget()
+
+        view_log_tab = EntryReaderWidget()
+        entry_tab = WLEntryWidget()
+
+        # Add layouts to TabWidget
+        tab_controller.addTab(view_log_tab, 'Work Log Reader')
+        tab_controller.addTab(entry_tab, 'Work Log Entry')
+
+
+        layout_frame = qtw.QHBoxLayout()
+        layout_frame.addWidget(tab_controller)
+
+        self.setLayout(layout_frame)
+
+
+class EntryReaderWidget(qtw.QTabWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
+
+
+class WLEntryWidget(qtw.QTabWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        entry_layout = qtw.QVBoxLayout()
+        
+        save_button = qtw.QPushButton('Save') #call db logic
+        clear_button = qtw.QPushButton('Clear')
+        self.entry_texbox = qtw.QTextEdit()
+        self.entry_texbox.setMaximumWidth(600)
+        self.entry_texbox.setFontPointSize(10)
+        self.entry_texbox.setAlignment(qtc.Qt.AlignLeft)
+        save_button.setFixedWidth(150)
+        clear_button.setFixedWidth(150)
+
+        button_layout = qtw.QHBoxLayout()
+        button_layout.addWidget(clear_button)
+        button_layout.addWidget(save_button)
+
+        entry_layout.addLayout(button_layout)
+        entry_layout.addWidget(self.entry_texbox)
+
+        self.setLayout(entry_layout)
+
 
 
 
